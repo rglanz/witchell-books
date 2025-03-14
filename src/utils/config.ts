9aa2405cdce9ee3c7b2443dde333ab2ce218271b
Ryan <glanzrm@gmail.com>
2025-03-14 13:59:10 -0500

    adds preorder link

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,5 +3,7 @@ const siteUrl = "https://witchellbooks.com/";
 const siteDescription =
   "Witchell Books transports young readers to a world steeped in magic and old folklore.";
 const ogImagePath = "/og-image.png";
+const preOrderUrl =
+  "https://www.barnesandnoble.com/w/vicki-voland-and-the-goblins-ghosts-ghouls-s-p-witchell/1147023056";
 
-export { siteName, siteUrl, siteDescription, ogImagePath };
+export { siteName, siteUrl, siteDescription, ogImagePath, preOrderUrl };
